ee52d4cecab37fd3a5a283f029e6b6a8ce6eaaba
Kamaliki Clive <justuskamaliki@gmail.com>
2023-06-06 09:06:10 +0300

    regular expression

--- getdomain.js
+++ getdomain.js
@@ -1,5 +1,9 @@
 //i hace a url either http or https and i want to get the domain name from it
 
 function getDomain(url) {
-    return url.replace(/(http|https):\/\/(www.)?/, "").split(".")[0];
+    return url.replace(/(http:\/\/|https:\/\/)?(www\.)?/, "").split(".")[0];
 }
+
+const url = "www.xakep.ru";
+
+console.log(getDomain(url)); // xakep
